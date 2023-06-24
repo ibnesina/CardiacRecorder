6d58805ae736b9e22bf65ae392a9563ef39d7cfa
md.ibnesina <ibnesina10@gmail.com>
2023-06-24 09:41:11 +0600

    Registration Activity Created

--- app/src/main/java/com/example/cardiacrecorder/SplashScreenActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/SplashScreenActivity.java
@@ -24,7 +24,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
@@ -68,11 +68,11 @@ public class SplashScreenActivity extends AppCompatActivity {
     }
     public void statApp()
     {
-        //if(FirebaseAuth.getInstance().getCurrentUser()==null) {
-           // Intent intent=new Intent(SplashScreenActivity.this, RegisterActivity.class);
-            //startActivity(intent);
+        if(FirebaseAuth.getInstance().getCurrentUser()==null) {
+            Intent intent=new Intent(SplashScreenActivity.this, RegisterActivity.class);
+            startActivity(intent);
             finish();
-       // }
+        }
         //else {
            // startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
           //  finish();
