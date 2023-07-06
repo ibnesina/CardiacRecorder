93220d4369c6e158c8bf6fc9d4a80b46e023b8e4
md.ibnesina <ibnesina10@gmail.com>
2023-07-06 15:29:21 +0600

    Some Modifications

--- app/src/test/java/com/example/cardiacrecorder/RecordAddDeleteUnitTest.java
+++ app/src/test/java/com/example/cardiacrecorder/RecordAddDeleteUnitTest.java
@@ -23,90 +23,90 @@ public class RecordAddDeleteUnitTest {
     /**
      * Testing addRecord method
      */
-//    @Test
-//    public void testAddRecord() {
-//        RecordList recordList = new RecordList();
-//
-//        RecordModel recordModel1 = new RecordModel("65", "80", "120", "Walking", Timestamp.now());
-//        recordList.addRecord(recordModel1);
-//        assertEquals(1, recordList.arrayList.size());
-//
-//        RecordModel recordModel2 = new RecordModel("70", "60", "90", "Resting", Timestamp.now());
-//        recordList.addRecord(recordModel2);
-//        assertEquals(2, recordList.arrayList.size());
-//    }
-//
-//    /**
-//     * testing addRecord method for exceptions
-//     */
-//    @Test
-//    public void testAddRecordException() {
-//        RecordList recordList = new RecordList();
-//
-//        RecordModel recordModel1 = new RecordModel("65", "80", "120", "Walking", Timestamp.now());
-//        recordList.addRecord(recordModel1);
-//        assertThrows(IllegalArgumentException.class, ()-> recordList.addRecord(recordModel1));
-//    }
-//
-//    /**
-//     * testing deleteRecord method
-//     */
-//    @Test
-//    public void testDeleteRecord() {
-//        RecordList recordList = new RecordList();
-//
-//        RecordModel recordModel1 = new RecordModel("70", "80", "120", "Walking", Timestamp.now());
-//        recordList.addRecord(recordModel1);
-//        assertEquals(1, recordList.arrayList.size());
-//        assertTrue(recordList.arrayList.contains(recordModel1));
-//
-//        RecordModel recordModel2 = new RecordModel("65", "60", "90", "Resting", Timestamp.now());
-//        recordList.addRecord(recordModel2);
-//        assertEquals(2, recordList.arrayList.size());
-//        assertTrue(recordList.arrayList.contains(recordModel2));
-//
-//        RecordModel recordModel3 = new RecordModel("75", "80", "130", "Working", Timestamp.now());
-//        recordList.addRecord(recordModel3);
-//        assertEquals(3, recordList.arrayList.size());
-//        assertTrue(recordList.arrayList.contains(recordModel3));
-//
-//        recordList.deleteRecord(1);
-//        assertEquals(2, recordList.arrayList.size());
-//        assertTrue(!recordList.arrayList.contains(recordModel2));
-//
-//        recordList.deleteRecord(0);
-//        assertEquals(1, recordList.arrayList.size());
-//        assertTrue(!recordList.arrayList.contains(recordModel1));
-//    }
-//
-//    /**
-//     * testing deleteRecord method for exceptions
-//     */
-//    @Test
-//    public void testDeleteRecordException() {
-//        RecordList recordList = new RecordList();
-//
-//        RecordModel recordModel1 = new RecordModel("70", "80", "120", "Walking", Timestamp.now());
-//        recordList.addRecord(recordModel1);
-//        assertEquals(1, recordList.arrayList.size());
-//        assertTrue(recordList.arrayList.contains(recordModel1));
-//
-//        assertThrows(IllegalArgumentException.class, ()-> recordList.deleteRecord(1));
-//    }
-//
-//    /**
-//     * testing count method
-//     */
-//    @Test
-//    public void testCountRecord() {
-//        RecordList recordList = new RecordList();
-//
-//        RecordModel recordModel1 = new RecordModel("70", "80", "120", "Walking", Timestamp.now());
-//        recordList.addRecord(recordModel1);
-//        assertEquals(1, recordList.count());
-//
-//        RecordModel recordModel2 = new RecordModel("65", "60", "90", "Resting", Timestamp.now());
-//        recordList.addRecord(recordModel2);
-//        assertEquals(2, recordList.count());
-//    }
+    @Test
+    public void testAddRecord() {
+        RecordList recordList = new RecordList();
+
+        RecordModel recordModel1 = new RecordModel("65", "80", "120", "Walking", Timestamp.now());
+        recordList.addRecord(recordModel1);
+        assertEquals(1, recordList.arrayList.size());
+
+        RecordModel recordModel2 = new RecordModel("70", "60", "90", "Resting", Timestamp.now());
+        recordList.addRecord(recordModel2);
+        assertEquals(2, recordList.arrayList.size());
+    }
+
+    /**
+     * testing addRecord method for exceptions
+     */
+    @Test
+    public void testAddRecordException() {
+        RecordList recordList = new RecordList();
+
+        RecordModel recordModel1 = new RecordModel("65", "80", "120", "Walking", Timestamp.now());
+        recordList.addRecord(recordModel1);
+        assertThrows(IllegalArgumentException.class, ()-> recordList.addRecord(recordModel1));
+    }
+
+    /**
+     * testing deleteRecord method
+     */
+    @Test
+    public void testDeleteRecord() {
+        RecordList recordList = new RecordList();
+
+        RecordModel recordModel1 = new RecordModel("70", "80", "120", "Walking", Timestamp.now());
+        recordList.addRecord(recordModel1);
+        assertEquals(1, recordList.arrayList.size());
+        assertTrue(recordList.arrayList.contains(recordModel1));
+
+        RecordModel recordModel2 = new RecordModel("65", "60", "90", "Resting", Timestamp.now());
+        recordList.addRecord(recordModel2);
+        assertEquals(2, recordList.arrayList.size());
+        assertTrue(recordList.arrayList.contains(recordModel2));
+
+        RecordModel recordModel3 = new RecordModel("75", "80", "130", "Working", Timestamp.now());
+        recordList.addRecord(recordModel3);
+        assertEquals(3, recordList.arrayList.size());
+        assertTrue(recordList.arrayList.contains(recordModel3));
+
+        recordList.deleteRecord(1);
+        assertEquals(2, recordList.arrayList.size());
+        assertTrue(!recordList.arrayList.contains(recordModel2));
+
+        recordList.deleteRecord(0);
+        assertEquals(1, recordList.arrayList.size());
+        assertTrue(!recordList.arrayList.contains(recordModel1));
+    }
+
+    /**
+     * testing deleteRecord method for exceptions
+     */
+    @Test
+    public void testDeleteRecordException() {
+        RecordList recordList = new RecordList();
+
+        RecordModel recordModel1 = new RecordModel("70", "80", "120", "Walking", Timestamp.now());
+        recordList.addRecord(recordModel1);
+        assertEquals(1, recordList.arrayList.size());
+        assertTrue(recordList.arrayList.contains(recordModel1));
+
+        assertThrows(IllegalArgumentException.class, ()-> recordList.deleteRecord(1));
+    }
+
+    /**
+     * testing count method
+     */
+    @Test
+    public void testCountRecord() {
+        RecordList recordList = new RecordList();
+
+        RecordModel recordModel1 = new RecordModel("70", "80", "120", "Walking", Timestamp.now());
+        recordList.addRecord(recordModel1);
+        assertEquals(1, recordList.count());
+
+        RecordModel recordModel2 = new RecordModel("65", "60", "90", "Resting", Timestamp.now());
+        recordList.addRecord(recordModel2);
+        assertEquals(2, recordList.count());
+    }
 }
